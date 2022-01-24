ee4040a2e4c845b0b9acb56058d587cd9d4a4ac2
Arthur Kappes <artkappes@gmail.com>
2022-01-24 15:05:38 -0300

    feat: Adicionado migration para entidade frutas

---
+++ database/migrations/2022_01_24_160507_create_fruits_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFruitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fruits', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->decimal('price', 10, 2);
+            $table->date('expiration_at');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fruits');
+    }
+}
